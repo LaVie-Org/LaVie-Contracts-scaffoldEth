880563e1f4845c5b92a12c096bf574b2e0affd4b
Christian Martinez <christianbm1@gmail.com>
2021-09-22 14:32:01 -0400

    modified game and items contract

--- packages/hardhat/contracts/Game.sol
+++ packages/hardhat/contracts/Game.sol
@@ -14,7 +14,31 @@ contract Game is Ownable {
         items = _items;
     }
 
-    function gameCreateAccount(address player, string memory playerStateURI) public onlyOwner returns(uint) {
+    function newPlayer(address player, uint256 accountType, string memory playerStateURI) public payable {
+        //1. take stake
+        //2. create account
+        //3. assign items
+    }
+
+    function createPlayerAccount(address player, string memory playerStateURI) internal onlyOwner returns(uint) {
         return accounts.createAccount(player, playerStateURI);
+    }
+
+    function assignItemsToPlayer(address player, uint256 accountType) internal onlyOwner {
+        if(accountType == 3){
+            //assuming items are pre-minted
+            //initiate w/ 3 basic items and 1 rare
+            //knife, brass knuckles, bat
+            //rare item should be random? Init chainlink random #
+            items.safeBatchTransferFrom(items, player, [1, 5, 9], [1, 1, 1], "");
+        } else if (accountType == 2){
+            //assuming items are pre-minted
+            //initiate w/ 2 basic items
+            //knife, bat
+            items.safeBatchTransferFrom(items, player, [1, 9], [1, 1], "");
+        } else {
+            //skip
+            //basic accounts, tier 1 do not get any items.
+        }
     }
 }
