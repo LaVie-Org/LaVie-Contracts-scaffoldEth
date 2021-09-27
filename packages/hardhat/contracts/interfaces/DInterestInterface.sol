664ad51605689b72c15e8e228ccd3f836bc01759
Quentin <thealmightyquinn@hotmail.com>
2021-09-27 14:36:36 +0200

    Update DInterestInterface.sol

--- packages/hardhat/contracts/interfaces/DInterestInterface.sol
+++ packages/hardhat/contracts/interfaces/DInterestInterface.sol
@@ -1,13 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract DInterestInterface {
+interface DInterestInterface {
+    function deposit(uint256 depositAmount, uint64 maturationTimestamp)
+        external
+        returns (uint64 depositID);
 
-  function deposit(uint256 depositAmount, uint64 maturationTimestamp) public returns (uint64 depositID){}
-
-   function withdraw(
+    function withdraw(
         uint64 depositID,
         uint256 virtualTokenAmount,
         bool early
-    )public returns (uint256 withdrawnStablecoinAmount) {}
-} 
+    ) external returns (uint256 withdrawnStablecoinAmount);
+}
